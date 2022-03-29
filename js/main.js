7ca977b13ae381c01e7feaad05d9da75edfeaf58
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-03-29 16:59:25 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,3 +19,37 @@ for (let i = 0; i < marqueeElementsDisplayed; i++) {
     marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
     // marqueeContent2.appendChild(marqueeContent2.children[i].cloneNode(true));
 }
+
+if ($('.items-services').length > 0) {
+    $('.items-services').css('opacity', '0');
+    setTimeout(function () {
+
+        $('.items-services').css('opacity', '1');
+        let listItems = gsap.utils.toArray(".items-services .item-services");
+        listItems.forEach(element => {
+            gsap.from(element, {
+                opacity: 0
+            }),
+                gsap.to(element, {
+                    opacity: 1,
+                    duartion: 1,
+                    scrollTrigger: {
+                        trigger: element,
+                        markers: false,
+                        start: "top 80%",
+                        end: "bottom 90%",
+                        toggleActions: 'play none none reverse',
+                    }
+                });
+        });
+        setTimeout(function () {
+            $('.items-services .item-services').css('opacity', '0');
+        }, 1800);
+    }, 500);
+
+}
+
+setTimeout(function () {
+    //$('.background').css('opacity', '0');
+    $('.background').first().css('opacity', '1');
+}, 1800);
